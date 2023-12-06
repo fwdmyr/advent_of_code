d68702e5dc49f7163c0b339adc61a7d6303ff336
felix <ftwidmayer@gmail.com>
2023-12-06 14:09:11 +0100

    Day 3: done

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -2,11 +2,12 @@ use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
 
+use itertools::Itertools;
+
 #[derive(Debug, PartialEq)]
 enum Token {
-    Number(usize, usize),
-    Gear,
-    Symbol,
+    Number((usize, usize), usize),
+    Gear(Option<usize>),
 }
 
 fn build_frame(pos: (usize, usize), width: usize) -> Vec<(usize, usize)> {
@@ -26,12 +27,25 @@ fn build_frame(pos: (usize, usize), width: usize) -> Vec<(usize, usize)> {
 
 fn validate_token(tokens: &HashMap<(usize, usize), Token>, pos: (usize, usize)) -> Option<Token> {
     match tokens.get(&pos) {
-        Some(Token::Number(value, width)) => {
-            let frame = build_frame(pos, *width);
-            frame
+        Some(Token::Gear(None)) => {
+            let frame = build_frame(pos, 1);
+            let vals = frame
                 .iter()
-                .any(|fpos| tokens.get(&fpos).is_some_and(|t| t.eq(&Token::Symbol)))
-                .then(|| Token::Number(value.clone(), width.clone()))
+                .filter_map(|fpos| {
+                    if let Some(Token::Number(pos, val)) = tokens.get(&fpos) {
+                        Some((pos, val))
+                    } else {
+                        None
+                    }
+                })
+                .unique_by(|data| data.0)
+                .map(|data| data.1.clone())
+                .collect::<Vec<usize>>();
+            if vals.len() == 2 {
+                Some(Token::Gear(Some(vals.into_iter().product())))
+            } else {
+                None
+            }
         }
         _ => None,
     }
@@ -47,18 +61,22 @@ fn parse_line(row: usize, line: &str) -> Vec<((usize, usize), Token)> {
         if ch.is_ascii_digit() {
             digit.push(ch);
         } else if let Ok(d) = digit.parse::<usize>() {
-            for j in 0..=digit.len() {
-                digits.push(((row, i - j), Token::Number(d, digit.len())));
+            for j in 1..=digit.len() {
+                let pos = (row, i - digit.len());
+                digits.push(((row, i - j), Token::Number(pos, d)));
             }
             digit.clear();
         }
         if ch.eq(&'*') {
-            digits.push(((row, i), Token::Gear));
+            digits.push(((row, i), Token::Gear(None)));
         }
     }
 
     if let Ok(d) = digit.parse::<usize>() {
-        digits.push(((row, 140 - digit.len()), Token::Number(d, digit.len())));
+        for j in 1..=digit.len() {
+            let pos = (row, 140 - digit.len());
+            digits.push(((row, 140 - j), Token::Number(pos, d)));
+        }
     }
 
     digits
@@ -82,7 +100,7 @@ fn main() -> io::Result<()> {
         .iter()
         .filter_map(|(pos, _)| validate_token(&schematic, pos.clone()))
         .fold(0, |acc, t| match t {
-            Token::Number(val, _) => acc + val,
+            Token::Gear(Some(val)) => acc + val,
             _ => acc,
         });
 
